5a01913b033686465a7ed872956ccd947200bcc5
cpcanfield <102257123+cpcanfield@users.noreply.github.com>
2022-04-27 17:22:25 -0500

    Creating classes for: tarot_card.cpp/.h, tarot_deck.cpp/.h, tarot_reading.cpp/.h

--- wisdom_seeker.cpp
+++ wisdom_seeker.cpp
@@ -1,5 +1,28 @@
-//
-// Created by Caroline Canfield on 4/27/22.
-//
+//Caroline Canfield: The_Mystic_Computer
+//CSCE 306; Spring 2022
+//driver file for the user (the wisdom seeker) class for the mystic computer program
+//wisdom_seeker.cpp
 
+
+#include <random>
+#include <iterator>
+#include <algorithm>
+#include <iomanip>
+#include <sstream>
+#include<vector>
+#include <iostream>
+#include<string>
+using namespace std;
 #include "wisdom_seeker.h"
+
+//constructor
+//will call the establish functions
+wisdom_seeker::wisdom_seeker(){}
+
+//setters
+void set_birthday(int, int){
+
+}
+void set_age(int);
+void set_pref_gender(string);
+void set_sign(string );
--- wisdom_seeker.h
+++ wisdom_seeker.h
@@ -4,9 +4,56 @@
 
 #ifndef THE_MYSTIC_COMPUTER_WISDOM_SEEKER_H
 #define THE_MYSTIC_COMPUTER_WISDOM_SEEKER_H
-
+#include <random>
+#include <iterator>
+#include <algorithm>
+#include <iomanip>
+#include <sstream>
+#include<vector>
+#include <iostream>
+#include<string>
+using namespace std;
 
 class wisdom_seeker {
+public:
+    //constructor
+    //will call the establish functions
+    wisdom_seeker();
+
+    //functions to figure out the users base card
+    string calc_zodiac_sign(int);
+    string suite_base_card();
+    string calc_base_card();
+
+    //setters
+    void set_seeker_name(string);
+    void set_birthday(int[]);
+    void set_age(int);
+    void set_pref_gender(string);
+    void set_sign(string );
+
+    //getters
+    int get_birthday();
+    int get_age();
+    string get_pref_gender();
+    string get_sign();
+    string set_seeker_name();
+
+
+private:
+    //variables for the user information
+    int birthday;
+    int age;
+    string pref_gender;
+    string sign;
+    string seeker_name;
+
+
+    //establish functions
+    void est_birthday(string);
+    void est_age(int);
+    void est_pref_gender(string);
+    void est_sign(string );
 
 };
 
